c82451bf43aaa65535cddb8474a060ba7a91aa50
henry <henry.bevil@PSCS.org>
2018-01-08 14:37:37 -0800

    a form and some php

---
+++ php1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>brain</title>
+</head>
+<body>
+  <form method="get">
+    <input type="text" name="a">
+    <input type="submit">
+  </form>
+  <?php echo GET["a"]; ?>
+</body>
+<footer>
+  <p>toes</p>
+</footer>
